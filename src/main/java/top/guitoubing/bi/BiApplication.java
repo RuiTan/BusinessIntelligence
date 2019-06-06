89c59ecd3adf3dc520730e6dd188482129631ed6
RuiTan <tanrui106@163.com>
2019-06-06 16:19:25 +0800

    init by tanrui at 6.6 16:19

---
+++ src/main/java/top/guitoubing/bi/BiApplication.java
@@ -0,0 +1,13 @@
+package top.guitoubing.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiApplication.class, args);
+    }
+
+}
