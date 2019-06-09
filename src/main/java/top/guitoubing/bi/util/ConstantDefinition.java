06d01dcf4fd0018470878825b7e6c19b4ab5c90b
RuiTan <tanrui106@163.com>
2019-06-10 00:08:33 +0800

    修改数据库服务器IP(路由器易重启导致IP变化) 6.10 00:07

--- src/main/java/top/guitoubing/bi/util/ConstantDefinition.java
+++ src/main/java/top/guitoubing/bi/util/ConstantDefinition.java
@@ -2,7 +2,7 @@ package top.guitoubing.bi.util;
 
 public class ConstantDefinition {
     // 连接字符串
-    static final String url = "bolt://180.160.38.37:7687";
+    static final String url = "bolt://58.41.206.35:7687";
     static final String username = "neo4j";
     static final String password = "tanrui";
 
